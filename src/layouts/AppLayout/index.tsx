3791b6b4c73ddc578d6b711505dc5e7cac932639
Saurabh Karna <karnasaurabh123@gmail.com>
2023-05-18 15:05:50 +0545

    add back navigation from detail page

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router'
 import { PageHeader } from 'antd'
+import { collections } from '../../data/collection'
 import './index.css'
 
 const AppLayout = ({ children }) => {
+
+  const navigate = useNavigate()
+  const { slug } = useParams()
+  const [itemName, setItemName] = useState(null)
+
+  useEffect(() => {
+    if (slug) {
+      const product = collections.filter((item) => item.slug === slug)
+      setItemName(product[0].name)
+    } else {
+      setItemName(null)
+    }
+  }, [slug])
+  
+
   return (
     <>
-        <PageHeader className="header" title="All items"></PageHeader>
-        <div className="card"> {children} </div>
+      {itemName ? (
+        <PageHeader
+          className="header"
+          title={<div className="title"> {itemName} </div>}
+          onBack={() => navigate('/')}
+        />
+      ) : (
+        <PageHeader
+          className="header"
+          title="All items"
+        />
+      )}
+      <div className="card"> {children} </div>
     </>
   )
 }
